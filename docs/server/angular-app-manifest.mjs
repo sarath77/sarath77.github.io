af244a3640d5d7a1cefc288a95746653cb023518
sarath77 <sarathc.rox@gmail.com>
2025-03-09 10:58:27 +0000

    updated style.css

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -7,8 +7,8 @@ export default {
   routes: undefined,
   entryPointToBrowserMapping: {},
   assets: {
-    'index.csr.html': {size: 782, hash: '6b8d9145fb9c36bdcdb43804d2e4b628175679d6cc6ef349974c32e44b6cf218', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1100, hash: '3023a703bc42651ecdc05dffcc6d5a3f2ebd851df8174b0df85368aab9ebd784', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 786, hash: '8e509b718b7ec86426c4921e300c942434168a44d2aa350b2f4ee3178aae6758', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1104, hash: 'aade54a0b42ae9eb4f6bf6a3e98b945b3608f75cbfc5db7991646906df9f9af9', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-U25NBIGL.css': {size: 347, hash: 'HIFGqdfxbxk', text: () => import('./assets-chunks/styles-U25NBIGL_css.mjs').then(m => m.default)}
   },
 };
